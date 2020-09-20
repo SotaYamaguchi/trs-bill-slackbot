52b3e8fc72ba40ff4e02d5b215f5ebb9d552cf87
Sota <s.yamaguchi@redimpulz.com>
2020-09-21 05:07:29 +0900

    フォーマット修正

--- functions/src/slack/commands/listCourse.ts
+++ functions/src/slack/commands/listCourse.ts
@@ -3,6 +3,26 @@ import { App } from "@slack/bolt";
 import { firestore } from "../../lib/firestore";
 import { Course } from '../../type/common';
 
+const createMessageBlock = (
+  courses: Course[]
+) => {
+  return [
+    {
+      type: "divider"
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: courses.map(x => `${x.course} ${x.place} ${x.price.toLocaleString()}円`).join('\n\n')
+      },
+    },
+    {
+      type: "divider"
+    }
+  ];
+};
+
 const getCourses = async (): Promise<Course[]> => {
   let courses = [] as Course[];
   try {
@@ -29,7 +49,8 @@ export const useListCourseCommand = (app: App) => {
       await app.client.chat.postMessage({
         token: context.botToken,
         channel: channelId,
-        text: courses.map(x => `${x.course} ${x.place} ${x.price.toLocaleString()}円`).join('\n\n')
+        text: "",
+        blocks: createMessageBlock(courses)
       });
     } catch (error) {
       console.error("post message error", error);
